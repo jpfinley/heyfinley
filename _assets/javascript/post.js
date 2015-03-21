75db84ccf512d7ccac025f213cf6623683e094ee
John Finley <jpfinley@gmail.com>
2015-03-21 12:45:58 -0400

    Color cleanup; SVG hover style

--- _assets/javascript/post.js
+++ _assets/javascript/post.js
@@ -1,5 +1,10 @@
+// Hide everything by default on page load
 $('.hideable').hide(0);
 
+// When scroll amount is above threshold,
+// turn on the nav and show all its elements.
+// When scroll amount is above,
+// hide the nav and its elements
 $(window).scroll(function() {
   if ($(this).scrollTop() > 110){
     $('nav.navbar-default').css({
@@ -17,6 +22,19 @@ $(window).scroll(function() {
   }
 });
 
+// SVG rollover effect. Nasty. Can't figure out
+// how else to change two fills with a single :hover
+$('.logo').hover(
+  function() {
+    $( '.logo-field'   ).css({ "fill": "#7F8C8D" }); // asbestos
+    $( '.logo-initial' ).css({ "fill": "#FFFFFF" });
+  }, function() {
+    $( '.logo-field'   ).css({ "fill": "#ECF0F1" }); // clouds
+    $( '.logo-initial' ).css({ "fill": "#E67E22" }); // carrot
+  }
+);
+
+// Scroll to top in breadcrumb click
 $('#tail').click(function(event) {
   event.preventDefault();
   $('html, body').animate({scrollTop: 0}, 'slow');
