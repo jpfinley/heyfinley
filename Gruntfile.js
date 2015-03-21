719c49e2f4997b17b7e7a214664a3dff34c4489c
John Finley <jpfinley@gmail.com>
2015-03-21 19:27:36 -0400

    Minify production JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,19 @@ module.exports = function(grunt) {
           ],
           dest: 'assets/javascript/site.js'
         }
+      },
+      uglify: {
+          build: {
+              src:  'assets/javascript/site.js',
+              dest: 'assets/javascript/site.min.js'
+          }
       }
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
+    // Load Grunt plug-ins
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
