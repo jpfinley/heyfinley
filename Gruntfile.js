3d3c8ff531c576eed42fd790d4dad5ece7e6e051
John Finley <jpfinley@gmail.com>
2015-03-21 22:48:01 -0400

    Don't generate sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,8 @@ module.exports = function(grunt) {
       sass: {
         dist: {
           options: {
-            style: 'compressed'
+            style: 'compressed',
+            sourcemap: 'none'
           },
           files: {
             'assets/css/build/main.css': 'assets/css/src/main.scss'
